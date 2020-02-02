814758b3865fd09c8bc9e8e2e2bdba20c20923cf
Ben Johnson <weagle08@gmail.com>
2020-02-01 21:24:47 -0600

    published

--- src/Auth.ts
+++ src/Auth.ts
@@ -4,12 +4,13 @@ import { Constants } from './Constants';
 import { IDto } from './IDto';
 import { ITokenPayload } from './ITokenPayload';
 import { IUserProfile } from './IUserProfile';
+import { LoginTheme } from './LoginTheme';
 
 export class Auth {
     private static _httpClient: HttpClient = new HttpClient();
 
-    public static getAuthUrl(clientId: number, responseType: 'code' | 'token', redirectUri: string, state: string): string {
-        return `${Constants.AUTH_BASE_URL}?client_id=${clientId}&response_type=${responseType}&scope=profile&redirect_uri=${redirectUri}&state=${state}`;
+    public static getAuthUrl(clientId: number, responseType: 'code' | 'token', redirectUri: string, state: string, theme: LoginTheme = LoginTheme.LIGHT): string {
+        return `${Constants.AUTH_BASE_URL}?client_id=${clientId}&response_type=${responseType}&scope=profile&redirect_uri=${redirectUri}&state=${state}&theme=${theme}`;
     }
 
     public static async getUserProfile(token: string): Promise<IUserProfile> {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './IUserProfile';
 export * from './Auth';
 export * from './ITokenPayload';
+export * from './LoginTheme';
